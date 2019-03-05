80cadd22fd8d65ee63a1773b17f5b5646dc1ee0e
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-03-05 20:15:35 +0100

    wip

--- src/state/actionTypes.js
+++ src/state/actionTypes.js
@@ -1,6 +1,6 @@
-export const DELETE_QUOTE = 'DELETE_QUOTE';
 export const MAKE_QUOTE_OF_THE_DAY = 'MAKE_QUOTE_OF_THE_DAY';
 export const ADD_QUOTE = 'ADD_QUOTE';
 export const ADD_QUOTES = 'ADD_QUOTES';
+export const DELETE_QUOTE = 'DELETE_QUOTE';
 export const SPINNER_ON = 'SPINNER_ON';
 export const SPINNER_OFF = 'SPINNER_OFF';
