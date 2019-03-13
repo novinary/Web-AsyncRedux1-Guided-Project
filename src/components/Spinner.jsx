b23685769d59ab29d67042f9254543bfaf1c73cb
novinary <novina.dev@gmail.com>
2019-03-13 10:45:43 +0000

    create mapStateToProps function of state that returns an object with a value of spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { bool } from 'prop-types';
 import styled from 'styled-components';
-
+import { connect } from 'react-redux';
 
 const StyledSpinner = styled.div`
-  /* implement so spinner and children DISPLAY when they should */
+/* implement so spinner and children DISPLAY when they should */
+.spinner {
+  display: ${props => (props.spinnerOn ? "block" : "none")};
+
+  .children {
+    display: ${props => {props.spinnerOn ? "none" : "block"}};
+  } 
 `;
 
 // connect this component to the store, to get the "spinner" slice of state
-export default class Spinner extends React.Component {
+export class Spinner extends React.Component {
   render() {
     return (
-      <StyledSpinner>
+      <StyledSpinner spiinerOn={this.props.spinner}>
         <div className='spinner'>Please Wait...</div>
         <div className='children'>{this.props.children}</div>
       </StyledSpinner>
@@ -23,4 +29,9 @@ Spinner.propTypes = {
   spinner: bool,
 };
 
+const mapStateToProps = function (state) {
+  return { spinner: state.spinner };
+};
+
 // default export the connected version!
+export default connect(mapStateToProps)(Spinner);g
